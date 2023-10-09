2b9663c386185f1cca1a02b259746c25e39dfccb
MJPDz <131334477+MJPDz@users.noreply.github.com>
2023-10-09 16:14:45 -0500

    custom hooks

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 // import { Contador } from "./components/Contador"
 // import { ListaNombres } from "./components/ListaNombres"
 import { Pokemones } from "./components/Pokemones"
+// import { Referencia } from "./components/Referencia"
 
 const App = () => {
   return (
     <>
       {/* <Texto name="Mako" apellido="Portugal" edad={22} />
       <hr />
-      <hr /> 
       <ListaNombres />
-      <Contador />*/}
+      <hr /> 
+      <Contador />
+      <hr />  */}
       <Pokemones />
+      {/* <hr /> 
+      <Referencia /> */}
     </>
   )
 }
--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -1,33 +1,20 @@
-import { useEffect, useState } from "react"
-
+import { useState } from "react"
+import { usePokemon } from "../hooks/usePokemon";
 
 export const Pokemones = () => {
 
-  const [pokemones, setPokemones] = useState([]);
-  const [anterior, setAnterior] = useState(null);
-  const [siguiente, setSiguiente] = useState(null);
   const [actual, setActual] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10");
-
-  useEffect(() => {
-    async function obtenerPokemones() {
-      const response = await fetch(actual);
-      const data = await response.json();
-      setPokemones(data.results);
-      setAnterior(data.previous);
-      setSiguiente(data.next);
-    }
-
-    obtenerPokemones()
-  }, [actual])
+  const { pokemones, loading, anterior, siguiente } = usePokemon(actual)
 
   return (
+    loading ? 
+      <p>Cargando pokemones</p>
+    :
     <div>
       <ul>
         {
           pokemones.map((pokemon, index) => {
-            return (
-              <li key={index}>{pokemon.name}</li>
-            )
+            return <li key={index}>{pokemon.name}</li>
           })
         }
       </ul>
